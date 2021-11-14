b315d22c7d77b9de717daf5dd6563b44756c91d8
Johannes Feige <johannes.feige@aclue.de>
2021-11-14 15:51:02 +0100

    fix uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'https://https://castronauts-server-jofe.herokuapp.com/',
+    uri: 'https://castronauts-server-jofe.herokuapp.com/',
     cache: new InMemoryCache(),
 });
 
